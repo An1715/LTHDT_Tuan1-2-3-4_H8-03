819d2fe2518d08f162f6a607a77a6cbca26bbdc0
An1715 <21015941.an@student.iuh.edu.vn>
2023-03-10 08:57:21 +0700

    tuan 4 commit

---
+++ src/NguyenDuyAn_21015941/DanhSachHangThucPham.java
@@ -0,0 +1,5 @@
+package NguyenDuyAn_21015941;
+
+public class DanhSachHangThucPham {
+
+}
---
+++ src/NguyenDuyAn_21015941/TestHTP.java
@@ -0,0 +1,11 @@
+package NguyenDuyAn_21015941;
+
+public class TestHTP {
+	public static void main(String[] args) {
+		HangThucPham HTP = new HangThucPham();
+		HTP.tieuDe();
+		System.out.println(HTP);
+	}
+	
+	
+}
